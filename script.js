05a59f96c95313feda62974f4e6878057a0908e5
LENIIIG072 <takeapassgame@gmail.com>
2026-05-11 18:54:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function login() {
   if (user === "معلمة أسماء" && pass === "Dx2hdSYX") {
     document.getElementById("login").style.display = "none";
     document.getElementById("adminPanel").style.display = "block";
+  // Mettre le lien GitHub ici
+    document.getElementById("downloadLink").href = "https://github.com/LENIIIG072/0QL4P4U03849/raw/main/Presentation%201.pptx";
   } else {
     alert("Accès refusé !");
   }
